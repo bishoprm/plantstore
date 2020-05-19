2ef385700dfb887079533646a3dcbdaa78c2dac9
bishoprm <bishop.rose@gmail.com>
2020-05-19 16:07:30 +0200

    setup routing

---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProductDetails() {
+  return <div>More details about the product here...</div>;
+}
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware, compose } from "redux";
 // import ReduxThunk from "redux-thunk";
 import reducer from "./reducer";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-// const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
+const enhancer = compose(devTools);
 
-const store = createStore(reducer, composeEnhancers);
+const store = createStore(reducer, enhancer);
 
 export default store;
