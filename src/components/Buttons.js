2a1f53ac0ae9e57624459143830697df88fe006b
Kruxk <reinko.brink@gmail.com>
2020-05-20 12:23:54 +0200

    fixed mistake with last commit

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectNumOfItems } from "../store/cart/selectors";
-import { addToCart, rmFromCart } from "../store/cart/actions";
+import { selectNumOfItem } from "../store/cart/selectors";
 
 function Buttons(props) {
   const dispatch = useDispatch();
-  const numOfItems = useSelector(selectNumOfItems(props.id));
+  const numOfItems = useSelector(selectNumOfItem(props.id));
 
   return (
     <div>
       <button
         className="buttons"
-        onClick={() => dispatch(rmFromCart(props.id, props.price))}
+        /* onClick={() => dispatch(rmFromCart(props.id, props.price))} */
       >
         -
       </button>
       <span>{numOfItems} in cart</span>
       <button
         className="buttons"
-        onClick={() => dispatch(addToCart(props.id, props.price))}
+        /* onClick={() => dispatch(addToCart(props.id, props.price))} */
       >
         +
       </button>
