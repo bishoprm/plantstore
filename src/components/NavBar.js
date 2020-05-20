d4afb9e7d116fef9565a9778023937f3bc317a89
Kruxk <reinko.brink@gmail.com>
2020-05-20 12:22:39 +0200

    changed selectnumofitems to be selectnumofitem

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export default function topBarCartInfo() {
   return (
-    <div>
+    <div className="navBar">
       <div align="right">
         <NavLink to="/Cart">0 products in cart</NavLink>
       </div>
--- src/store/cart/selectors.js
+++ src/store/cart/selectors.js
@@ -1,7 +1,15 @@
-export const selectNumOfItems = (id) => (state) => {
+export const selectNumOfItem = (id) => (state) => {
   const filteredProducts = state.cart.productIds.filter(
     (productId) => productId === id
   );
 
   return filteredProducts.length;
+};
+
+export const selectNumOfItems = (state) => {
+  if (state.cart.productIds === []) {
+    return 0;
+  } else {
+    return state.cart.productIds.length;
+  }
 };
